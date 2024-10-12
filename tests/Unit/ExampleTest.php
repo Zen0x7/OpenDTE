b755bd848496af4458d9b137dddd6829963cca85
Ian Torres <iantorres@outlook.com>
2024-10-12 03:19:50 -0300

    Testing añadido.

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
